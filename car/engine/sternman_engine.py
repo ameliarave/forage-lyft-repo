fd3984bea6fe9702f59f5e91276a9f0e900a8542
ameliarave <121872399+ameliarave@users.noreply.github.com>
2023-02-05 16:58:55 +1030

    Update sternman_engine.py

--- car/engine/sternman_engine.py
+++ car/engine/sternman_engine.py
@@ -1,16 +1,12 @@
-from abc import ABC
-#from serviceable import Serviceable
-#from car.car import Car
 from car.engine.engine import Engine
 
 
-class SternmanEngine(Engine, ABC):
+class SternmanEngine(Engine):
     def __init__(self, warning_light_is_on):
         super().__init__()
         self.__warning_light_is_on = warning_light_is_on
 
     def needs_service(self):
-
         if self.__warning_light_is_on:
             return True
         else:
