9975c4dd5caad3ebd57a33ba5ab624767dc3c837
ameliarave <121872399+ameliarave@users.noreply.github.com>
2023-02-05 17:01:11 +1030

    Update willoughby_engine.py

--- car/engine/willoughby_engine.py
+++ car/engine/willoughby_engine.py
@@ -1,11 +1,7 @@
-from abc import ABC
-#from serviceable import Serviceable
-#from car.car import Car
 from car.engine.engine import Engine
 
-class WilloughbyEngine(Engine, ABC):
+class WilloughbyEngine(Engine):
     def __init__(self, last_service_mileage, current_mileage):
-        super().__init__()
         self.__current_mileage = current_mileage
         self.__last_service_mileage = last_service_mileage
 
