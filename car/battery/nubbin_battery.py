4ed642835319b07ee045509ec72e5d0cf4ee178a
ameliarave <121872399+ameliarave@users.noreply.github.com>
2023-02-05 17:04:51 +1030

    Update nubbin_battery.py

--- car/battery/nubbin_battery.py
+++ car/battery/nubbin_battery.py
@@ -1,10 +1,8 @@
-from abc import ABC
 from car.battery.battery import Battery
 from datetime import datetime
 
-class NubbinBattery(Battery, ABC):    # once every 4 years
+class NubbinBattery(Battery):    # once every 4 years
     def __init__(self, last_service_date, current_date):
-        super().__init__()
         self.__last_service_date = last_service_date
         self.__current_date = current_date
     
